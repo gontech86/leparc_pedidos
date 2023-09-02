c1fcba085d84e6293b3c8bbb168fb55b5afa7ce8
Gonzalo ARNONE <arnonegonzalo@gmail.com>
2023-09-02 19:25:16 -0300

    Modularizacion de la creacion de la tabla.
    Separa la creacion del enzabezado en donde se agregan columnas y lo mismo para el resto de las filas, primera columna un Input para poner cantidad de productos y ultima columna se obtendra un subtotal

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-const rutaListaLocal = "./data/lista1.csv";
+const rutaListaLocal = "./data/lista.csv";
 
 function loadCSVFromFile() {
     fetch(rutaListaLocal)
@@ -22,7 +22,7 @@ function loadCSVFromFile() {
 loadCSVFromFile();
 
 
-function leerCSVInput() {
+function loadCSVFromInput() {
     // Obtener el archivo CSV seleccionado
     const archivo = document.querySelector("input[name='archivo']")
         .files[0];
@@ -32,7 +32,7 @@ function leerCSVInput() {
     reader.addEventListener("load", (event) => {
         // Convertir el contenido del archivo CSV en un array
         const datos = event.target.result.split("\n");
-        
+
         const divErrorArchivo = document.querySelector(".archivo-error");
         divErrorArchivo.classList.add("ocultar");
         crearTabla(datos);
@@ -45,13 +45,54 @@ function crearTabla(datos) {
     const tabla = document.getElementById("tabla");
     tabla.innerHTML = "";
 
-    // Iterar por los datos del archivo CSV
-    for (let i = 0; i < datos.length; i++) {
+    crearEncabezados(datos[0].split(","), tabla);
+    crearFilasConDatos(datos.slice(1), tabla);
+}
+
+function crearEncabezados(filaEncabezados, tabla) {
+    // Crear una fila en la tabla
+    const filaElemento = document.createElement("tr");
+
+    // Agrega encabezado Cantidad
+    const columnCantidad = document.createElement("th");
+    columnCantidad.textContent = "Cantidad";
+    filaElemento.appendChild(columnCantidad);
+
+    // Agregar las columnas que trae el archivo CSV a la fila
+    for (let j = 0; j < filaEncabezados.length; j++) {
+        const columna = document.createElement("th");
+        columna.textContent = filaEncabezados[j];
+        filaElemento.appendChild(columna);
+    }
+
+    // Agrega encabezado Subtotal
+    const columnSubtotal = document.createElement("th");
+    columnSubtotal.textContent = "Subtotal";
+    filaElemento.appendChild(columnSubtotal);
+
+    // Agregar la fila a la tabla
+    tabla.appendChild(filaElemento);
+}
+
+function crearFilasConDatos(filaDatos, tabla) {
+
+    // Iterar por los datos del archivo CSV sin el encabezado
+    for (let i = 0; i < filaDatos.length; i++) {
         // Obtener la fila actual del archivo CSV
-        const fila = datos[i].split(",");
+        const fila = filaDatos[i].split(",");
 
         // Crear una fila en la tabla
         const filaElemento = document.createElement("tr");
+
+        // Agregar input de numeros enteros primera columna
+        const cantidadInput = document.createElement('input');
+        cantidadInput.type = 'number';
+        cantidadInput.value = 0;
+        cantidadInput.min = 0; // Valor mínimo permitido
+        cantidadInput.pattern = "\\d+"; // Patrón para números enteros
+        const tdCantidad = document.createElement('td');
+        tdCantidad.appendChild(cantidadInput);
+        filaElemento.appendChild(tdCantidad);
 
         // Agregar las columnas a la fila
         for (let j = 0; j < fila.length; j++) {
@@ -60,10 +101,15 @@ function crearTabla(datos) {
             filaElemento.appendChild(columna);
         }
 
+        //Agrego ultima columna a la fila con Subtotal
+        const tdCantidad2 = document.createElement('td');
+        tdCantidad2.textContent = "0";
+        filaElemento.appendChild(tdCantidad2);
+
         // Agregar la fila a la tabla
         tabla.appendChild(filaElemento);
     }
 }
 
 // Agrega el evento change al elemento input
-document.querySelector("input[name='archivo']").addEventListener("change", leerCSVInput);
+document.querySelector("input[name='archivo']").addEventListener("change", loadCSVFromInput);
